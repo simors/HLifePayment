3f120598bb3e55294bcd2056f859e713f474cbf5
wanpeng <wanpeng19860919@163.com>
2017-03-30 15:24:48 +0800

    <feat> use express app

--- index.js
+++ index.js
@@ -1,44 +1,21 @@
-var http = require('http');
+'use strict';
+var AV = require('leancloud-storage');
 
-http.createServer(function (req, res) {
+AV.init({
+  appId: 'K5Rltwmfnxd5pYjMsOFFL0kT-gzGzoHsz',
+  appKey: 'UseC5jvqLT7TIiQWI8nRPmEl',
+});
 
-  req.setEncoding('utf8');
-  var postData = "";
-  req.addListener("data", function (chunk) {
-    postData += chunk;
+var app = require('./app');
+
+app.listen(3000, function () {
+  console.log('Node app is running, port:', 3000);
+
+  // 注册全局未捕获异常处理器
+  process.on('uncaughtException', function(err) {
+    console.error("Caught exception:", err.stack);
   });
-  req.addListener("end", function () {
-    var resp = function (ret, status_code) {
-      res.writeHead(status_code, {
-        "Content-Type": "text/plain; charset=utf-8"
-      });
-      res.end(ret);
-    }
-    try {
-      var event = JSON.parse(postData);
-      if (event.type === undefined) {
-        return resp('Event 对象中缺少 type 字段', 400);
-      }
-      switch (event.type) {
-        case "charge.succeeded":
-          // 开发者在此处加入对支付异步通知的处理代码
-          console.log("get Event: charge.succeeded")
-          return resp("OK", 200);
-          break;
-        case "refund.succeeded":
-          // 开发者在此处加入对退款异步通知的处理代码
-          console.log("get Event: refund.succeeded")
-          return resp("OK", 200);
-          break;
-        default:
-          console.log("get Event: 未知 Event 类型")
-          return resp("未知 Event 类型", 400);
-          break;
-      }
-    } catch (err) {
-      return resp('JSON 解析失败', 400);
-    }
+  process.on('unhandledRejection', function(reason, p) {
+    console.error("Unhandled Rejection at: Promise ", p, " reason: ", reason.stack);
   });
-}).listen(3000, "127.0.0.1");
-
-console.log('Server running on port 3000.');
+});
