fb5a35bedeae1530597626bf0399bf3e8dc91f76
wanpeng <wanpeng19860919@163.com>
2017-04-27 12:12:51 +0800

    <feat> add env params

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 var AV = require('leancloud-storage');
+var env = process.argv[2];
+var LC_APP_ID = ""
+var LC_APP_KEY = ""
+var port = 3000
 
 const LC_DEV_APP_ID = 'K5Rltwmfnxd5pYjMsOFFL0kT-gzGzoHsz'
 const LC_DEV_APP_KEY = 'UseC5jvqLT7TIiQWI8nRPmEl'
@@ -10,15 +14,33 @@ const LC_PRE_APP_KEY = 'XYSEQ5K4FYtMp7P8HXT8Vz63'
 const LC_PRO_APP_ID = ''
 const LC_PRO_APP_KEY = ''
 
+if(env === 'dev') {
+  LC_APP_ID = LC_DEV_APP_ID
+  LC_APP_ID = LC_DEV_APP_KEY
+  port = 3001
+} else if(env === 'pre') {
+  LC_APP_ID = LC_PRE_APP_ID
+  LC_APP_KEY = LC_PRE_APP_KEY
+  port = 3000
+} else if(env === 'pro') {
+  LC_APP_ID = LC_PRO_APP_ID
+  LC_APP_KEY = LC_PRO_APP_KEY
+  port = 3002
+} else {
+  LC_APP_ID = LC_PRE_APP_ID
+  LC_APP_KEY = LC_PRE_APP_KEY
+  port = 3000
+}
+
 AV.init({
-  appId: LC_PRE_APP_ID,
-  appKey: LC_PRE_APP_KEY,
+  appId: LC_APP_ID,
+  appKey: LC_APP_ID,
 });
 
 var app = require('./app');
 
-app.listen(3000, function () {
-  console.log('Ljyd-pre app is running, port:', 3000);
+app.listen(port, function () {
+  console.log('Ljyd app is running', env, 'port:', port);
 
   // 注册全局未捕获异常处理器
   process.on('uncaughtException', function(err) {
