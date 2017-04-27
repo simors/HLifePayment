f63474fd19f25041569eef21eac25a0f8de8b1da
wanpeng <wanpeng19860919@163.com>
2017-04-27 11:39:41 +0800

    <feat> 增加开发环境、预上线环境和生产环境

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ var AV = require('leancloud-storage');
 const LC_DEV_APP_ID = 'K5Rltwmfnxd5pYjMsOFFL0kT-gzGzoHsz'
 const LC_DEV_APP_KEY = 'UseC5jvqLT7TIiQWI8nRPmEl'
 
-const LC_PRO_APP_ID = 'TUVjJ5HHNmopfJeREa4IcB1T-gzGzoHsz'
-const LC_PRO_APP_KEY = 'XYSEQ5K4FYtMp7P8HXT8Vz63'
+const LC_PRE_APP_ID = 'TUVjJ5HHNmopfJeREa4IcB1T-gzGzoHsz'
+const LC_PRE_APP_KEY = 'XYSEQ5K4FYtMp7P8HXT8Vz63'
+
+const LC_PRO_APP_ID = ''
+const LC_PRO_APP_KEY = ''
 
 AV.init({
-  appId: LC_DEV_APP_ID,
-  appKey: LC_DEV_APP_KEY,
+  appId: LC_PRE_APP_ID,
+  appKey: LC_PRE_APP_KEY,
 });
 
 var app = require('./app');
 
 app.listen(3000, function () {
-  console.log('Node app is running, port:', 3000);
+  console.log('Ljyd-pre app is running, port:', 3000);
 
   // 注册全局未捕获异常处理器
   process.on('uncaughtException', function(err) {
