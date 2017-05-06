f14509e36e95849b9f36c7d865c76860ca742dda
wanpeng <wanpeng19860919@163.com>
2017-05-06 10:55:15 +0800

    <feat> 修改服务端口号[300* -> 400*]

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var AV = require('leancloud-storage');
 var env = process.argv[2];
 var LC_APP_ID = ""
 var LC_APP_KEY = ""
-var port = 3000
+var port = 4000
 
 const LC_DEV_APP_ID = 'K5Rltwmfnxd5pYjMsOFFL0kT-gzGzoHsz'
 const LC_DEV_APP_KEY = 'UseC5jvqLT7TIiQWI8nRPmEl'
@@ -17,19 +17,19 @@ const LC_PRO_APP_KEY = 'qhIzQiFonde2yeaBdlcXHUDz'
 if(env === 'dev') {
   LC_APP_ID = LC_DEV_APP_ID
   LC_APP_KEY = LC_DEV_APP_KEY
-  port = 3001
+  port = 4001
 } else if(env === 'pre') {
   LC_APP_ID = LC_PRE_APP_ID
   LC_APP_KEY = LC_PRE_APP_KEY
-  port = 3000
+  port = 4000
 } else if(env === 'pro') {
   LC_APP_ID = LC_PRO_APP_ID
   LC_APP_KEY = LC_PRO_APP_KEY
-  port = 3002
+  port = 4002
 } else {
   LC_APP_ID = LC_PRE_APP_ID
   LC_APP_KEY = LC_PRE_APP_KEY
-  port = 3000
+  port = 4000
 }
 
 AV.init({
